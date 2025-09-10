d93f74c884192ae60f3bce443d855be42a9619db
unknown <playarisa24@gmail.com>
2025-09-10 14:52:20 +0700

    fix: fix minor bug in CSRF_TRUSTED_ORIGINS

--- football_news/settings.py
+++ football_news/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "arisa-raezzura-footballnews.pbp.cs.ui.ac.id"]
 
-CSRF_TRUSTED_ORIGINS = ["https://arisa-raezzura-footballnews.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["https://arisa-raezzura-footballnews.pbp.cs.ui.ac.id"]
 
 # Application definition
 
